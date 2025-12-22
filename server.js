dbf3faeca5ce2265c6e3ed9b7da9d3236188febc
hridoy-tech542 <arafathridoy01811@gmail.com>
2025-12-22 22:51:52 +0600

    MongoDB connection & deploy-ready changes

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use("/payments", paymentRoutes);
 
 
 // Port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
